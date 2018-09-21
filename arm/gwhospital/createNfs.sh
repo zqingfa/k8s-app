ad1a9b50851a87d0078b1fef9c2da7f5f3ea7072
zqingfa <344266686@qq.com>
2018-09-22 00:33:58 +0800

    修改PV和PVC的标签对应

---
+++ arm/gwhospital/createNfs.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+mkdir -p /data/gwhospital/01rabbitmq/rabbitmq
+mkdir -p /data/gwhospital/02svn/svn
+mkdir -p /data/gwhospital/03discovery/data
+mkdir -p /data/gwhospital/04config/data
+mkdir -p /data/gwhospital/05fastdfs/fastdfs
+mkdir -p /data/gwhospital/06redis/redis
+mkdir -p /data/gwhospital/07activemq/activemq
+mkdir -p /data/gwhospital/08mysql/mysql
+mkdir -p /data/gwhospital/09iomsgateway/data
+mkdir -p /data/gwhospital/10iomsums/data
+mkdir -p /data/gwhospital/11iomsmonitor/data
+mkdir -p /data/gwhospital/12iomsmq/data
+mkdir -p /data/gwhospital/13ui/www
